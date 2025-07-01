3bc2cc6925ef25d957b74a60cc10243bed55f801
Abdullah Safdar <sheikhsafdar381@gmail.com>
2025-07-01 22:13:58 +0500

    first commit

---
+++ config/db.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+
+const catched = global.mongoose
+
+
+if(!catched) {
+    catched = global.mongoose = { conn: null, promise: null }
+}
+
+
+async function connectDB(){
+
+    if(catched.conn){
+        return catched.conn
+    }
+
+    if(!catched.promise){
+        const opts = {
+            bufferCommands: false,
+        }
+
+        catched.promise = mongoose.connect(`${process.env.MONGODB_URI}/HMElectronics`, opts).then(mongoose=>{
+            return mongoose
+        })
+    }
+
+    catched.conn = await catched.promise
+    return catched.conn
+}
+
+export default connectDB;
