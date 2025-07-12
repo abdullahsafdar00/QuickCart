f64c0730577da82fc9e00c5707a6cdc86d54ac1d
Abdullah Safdar <sheikhsafdar381@gmail.com>
2025-07-12 21:19:41 +0500

    fill

--- config/inngest.js
+++ config/inngest.js
@@ -1,7 +1,6 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
 import User from "@/models/user";
-import { connect } from "mongoose";
 import Order from "@/models/order";
 
 // Create a client to send and receive events
